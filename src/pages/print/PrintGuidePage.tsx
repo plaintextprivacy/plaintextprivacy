1b10823bf889751c6705b8ce9eb9a6e4eb80ffab
kb <spidery.circus_1y@icloud.com>
2026-08-06 23:43:17 -0700

    add files

---
+++ src/pages/print/PrintGuidePage.tsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom'
+import type { AnyGuide, ChecklistSection, ChecklistItem } from '@/types/guide'
+import './print.css'
+
+export const PrintGuidePage = () => {
+
+  return (
+      <div>
+        PDFs!
+      </div>
+  )
+}
